cc00755fbbc7e400de6affa0829af45f898cbd5c
Macronaut123 <marcos.vinicius.eua@gmail.com>
2013-07-29 21:15:05 -0300

    inserindo projeto leitor_poesias

---
+++ js/core.js
@@ -0,0 +1,33 @@
+//ARQUIVO CORE//
+
+var poesiaAtual = 0;
+
+$(document).ready(function(){
+	console.log("--INITIALIZED--");
+	
+	$('.poesia_titulo').html(poesias_titulos[poesiaAtual]);
+	$('.poesia_box').html(poesias[poesiaAtual]);
+	
+})
+
+function mudarPoesia(direcao){
+	
+	console.log(poesiaAtual)
+	
+	switch(direcao){
+		case "direita":
+			if(poesiaAtual < poesias_titulos.length - 1){
+				poesiaAtual++;
+				$('.poesia_titulo').html(poesias_titulos[poesiaAtual]);
+				$('.poesia_box').html(poesias[poesiaAtual]);			
+			}
+		break;
+		case "esquerda":
+			if(poesiaAtual > 0){
+				poesiaAtual--;
+				$('.poesia_titulo').html(poesias_titulos[poesiaAtual]);
+				$('.poesia_box').html(poesias[poesiaAtual]);			
+			}
+		break;
+	}
+}
